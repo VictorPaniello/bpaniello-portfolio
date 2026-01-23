583b9ff1d0ff96f41ea312f478ff70fffe55b0b1
VictorPaniello <vpanielloc@uoc.edu>
2026-01-23 14:01:39 +0100

    Creating and cleaning up Profile

---
+++ src/content/profile.ts
@@ -0,0 +1,13 @@
+import type { Profile } from "../lib/schema";
+
+const profile: Profile = {
+    name: "Blanca Paniello",
+    title: "Dr. ",
+    email: "blancapaniello@blancapaniello.com",
+    location: "Barcelona",
+    bio: "BlaBlaBla",
+    orcidId: "",
+    researchInterests: [""],
+    education:,
+
+}
