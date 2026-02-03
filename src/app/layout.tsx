edb19c78eb6ad8a6194d248f62f8a9d4ca2675a2
VictorPaniello <vpanielloc@uoc.edu>
2026-02-03 13:05:00 +0100

    Favicon added finally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,9 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
 
     return (
     <html lang="en" suppressHydrationWarning>
+        <head>
+            <link rel="icon" type="image/x-icon" href="/public/favicon.png" />
+        </head>
         <body>
             <FloatingNavbar />
                 {children}
